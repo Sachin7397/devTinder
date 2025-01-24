67d74abadfc9ae99d8321d7c2edc9c1d0d8565dc
Sachin7397 <sachinskamble0910@gmail.com>
2025-01-24 15:29:31 +0530

    handling with multiple routes and next functiion

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:id", (req, res) => {
   //get data from server
+  console.log(req?.params);
   res.send({ firstName: "Sachin", age: 20 });
 });
 
@@ -15,14 +16,21 @@ app.delete("/user", (req, res) => {
   res.send("Data deleted successfully");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from server");
-});
+app.use(
+  "/test",
+  (req, res, next) => {
+    // res.send("Hello from server1");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello from server2");
+  }
+);
 
 app.use("/", (req, res) => {
   res.send("Home");
 });
 
 app.listen(7777, () => {
-  console.log("Server is successfully running on port 3333");
+  console.log("Server is successfully running on port 7777");
 });
